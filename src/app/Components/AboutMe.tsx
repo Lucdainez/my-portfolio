5c5143cf93b731e87a3e4fed7d902f7fa8cdd591
lucdainez <lucdainez@gmail.com>
2024-05-25 15:24:11 -0300

    feat: add page AboutMe but not finished

--- src/app/Components/AboutMe.tsx
+++ src/app/Components/AboutMe.tsx
@@ -1,7 +1,20 @@
+'use client';
 import Image from 'next/image';
 import ImageLuc from '../../../public/images/97261512.jpg';
+import { useState } from 'react';
+import Contact from './Contact';
 
 export default function AboutMe() {
+  const [contact, setContact] = useState(false);
+
+  const clickContact = () => {
+    if (contact === false) {
+      setContact(true)
+    } else {
+      setContact(false)
+    }
+  }
+
   return (
     <section className="flex">
       <section className="flex-auto border-2 border-solid rounded border-rose-500">
@@ -16,10 +29,17 @@ export default function AboutMe() {
           Olá, meu nome é Lucas! Sou Desenvolvedor full-Stack.
         </h2>
         <p className="text-center">Tenho 22 anos</p>
-
-        <p>CONTATOS</p>
-        <p>Minha trajetória</p>
+        <div className="flex flex-col justify-center mt-24">
+          <button onClick={clickContact}>
+            <p>CONTATOS</p>
+          </button>
+          {contact && <Contact/>}
+          <button>
+            <p>Minha trajetória</p>
+          </button>
+        </div>
       </section>
+      
     </section>
   );
 }
---
+++ src/app/Components/Contact.tsx
@@ -0,0 +1,12 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function Contact() {
+  return (
+    <aside>
+      
+      <button><FaWhatsapp />Whatsapp</button>
+      <button>Email</button>
+      <button>Linkedin</button>
+    </aside>
+  )
+}
