b9cc5e0cb5130d38eb0de896fdfbe5cbb3f99746
lucdainez <lucdainez@gmail.com>
2024-05-27 22:54:03 -0300

    feat: change color background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import AboutMe from './Components/AboutMe';
 
 export default function Home() {
   return (
     <main>
-      <AboutMe/>
+      <AboutMe />
     </main>
   );
 }
