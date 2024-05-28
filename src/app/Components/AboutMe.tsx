9e0e644dad289eab2d04cd2d3139cadc50d3d3a2
lucdainez <lucdainez@gmail.com>
2024-05-27 22:54:42 -0300

    feat: section AboutMe in progress

--- src/app/Components/AboutMe.tsx
+++ src/app/Components/AboutMe.tsx
@@ -3,43 +3,61 @@ import Image from 'next/image';
 import ImageLuc from '../../../public/images/97261512.jpg';
 import { useState } from 'react';
 import Contact from './Contact';
+import MoreAboutMe from './MoreAboutMe';
 
 export default function AboutMe() {
   const [contact, setContact] = useState(false);
+  const [moreAboutMe, setMoreAboutMe] = useState(false);
 
   const clickContact = () => {
     if (contact === false) {
-      setContact(true)
+      setContact(true);
     } else {
-      setContact(false)
+      setContact(false);
     }
-  }
+  };
+
+  const clickMoreAboutMe = () => {
+    if (moreAboutMe === false) {
+      setMoreAboutMe(true);
+    } else {
+      setMoreAboutMe(false);
+    }
+  };
 
   return (
     <section className="flex">
-      <section className="flex-auto border-2 border-solid rounded border-rose-500">
+      <section className="flex-auto">
         <Image
           src={ImageLuc}
-          className="rounded opacity-100 h-64 w-64 ml-5 mt-5"
+          className="rounded h-64 w-64 ml-5 mt-5"
           alt="retrato do Lucas"
         />
       </section>
-      <section className="flex-auto justify-center items-center content-center p-1.5 border-2 border-solid rounded border-rose-500">
+      <section className="flex-auto justify-center items-center content-center p-1.5 mr-16 rounded-lg bg-back mt-5">
         <h2 className="text-center">
-          Olá, meu nome é Lucas! Sou Desenvolvedor full-Stack.
+          <strong>Olá, meu nome é Lucas! Sou Desenvolvedor full-Stack!</strong>
         </h2>
-        <p className="text-center">Tenho 22 anos</p>
-        <div className="flex flex-col justify-center mt-24">
-          <button onClick={clickContact}>
+        <p className="text-center">
+          <strong>Tenho 22 anos</strong>
+        </p>
+        <div className="flex flex-col self-center mt-10 h-min w-28 m-auto">
+          <button
+            className="bg-sky-400/25 hover:bg-sky-400/50 text-slate-300 font-bold shadow-md shadow-sky-400/25 hover:shadow-sky-400/100 py-1 px-1 mb-5 rounded"
+            onClick={clickContact}
+          >
             <p>CONTATOS</p>
           </button>
-          {contact && <Contact/>}
-          <button>
-            <p>Minha trajetória</p>
+          {contact && <Contact />}
+          <button
+            className="bg-sky-400/25 hover:bg-sky-400/50 text-slate-300 font-bold shadow-md shadow-sky-400/25 hover:shadow-sky-400/100 py-1 px-1 rounded"
+            onClick={clickMoreAboutMe}
+          >
+            <p>MAIS SOBRE MIM</p>
           </button>
+          {moreAboutMe && <MoreAboutMe />}
         </div>
       </section>
-      
     </section>
   );
 }
