9938667972a54cef18c14dcc034f0695e380e252
lucdainez <lucdainez@gmail.com>
2024-06-10 18:35:22 -0300

    feat: Add background and width specific

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,13 +11,18 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'back-code': 'url(../../public/images/fundo.jpeg)'
       },
       colors: {
-        'back': '#306A8C'                
+        'back': '#306A8C',
+        'main':  '#2d6484'
       },
       boxShadow: {
         'box': ' 5px -5px 13px #2d6484, -5px 5px 13px #337094'
+      },
+      spacing: {
+        'window': '1400px'
       }
     }
   },
